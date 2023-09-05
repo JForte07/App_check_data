5b24d49d2dbbd5ee990a79ca6a9b95e5c17e728f
JForte07 <julia.forte@epfedu.fr>
2023-09-05 12:59:11 +0200

    test image

--- page_info.py
+++ page_info.py
@@ -7,7 +7,7 @@ def page_infos():
     st.header("Pour récupérer le fichier de migration sur Django, suivez ces étapes :")
 
     st.markdown("1. Allez d'abord dans le dossier des migrations.")
-    image = Image.open('Picture\django3.png')
+    image = Image.open('django3.png')
     st.image(image, caption='Django Etape 1', use_column_width=True)
 
     st.markdown("2. Cliquez sur la migration qui vous intéresse.")
